9a20fbfa7d44e3fae8ed893e63ca6256e23a59a0
vivianw1 <122054036+vivianw1@users.noreply.github.com>
2023-12-19 12:02:47 -0800

    Player Scene Complete

---
+++ player.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var speed = 400
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		velocity.y += 1
+	if Input.is_action_pressed("down"):
+		velocity.y -= 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+
+	if velocity.length() > 0:
+		position = velocity * delta		
